48aa2ab475d26d9c6d98b223c33179dfe285fdeb
Abdullah-319 <abdullahahmad319.319@gmail.com>
2024-03-27 00:04:45 +0500

    lab_ass done

---
+++ Lab_06/labTask4.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main()
+{   
+    //Task1
+    	
+    //string instruction = "pwd";
+    //const char *command = instruction.c_str();
+    //system(command);
+
+
+
+    //Task2
+
+    //string instruction = "ls -l";
+    //const char *command = instruction.c_str();
+    //system(command);
+    
+    
+    
+    //Task3
+        
+    //string instruction = "date";
+    //const char *command = instruction.c_str();
+    //system(command);
+    
+    
+    
+    
+    //Task4
+    
+    //string skills = "cat > skills.txt <<EOF\n"
+    //                 "Name: Abdullah Ahmad\n"
+    //                 "Roll No: SP22-BCS-045\n"
+    //                 "Skills:\n"
+    //                 "1. Python programming\n"
+    //                 "2. Data analysis\n"
+    //                 "3. Machine learning\n"
+    //                 "4. Web development\n"
+    //                 "5. Problem solving\n"
+    //                 "6. Communication\n"
+    //                 "7. Leadership\n"
+    //                 "8. Time management\n"
+    //                 "9. Teamwork\n"
+    //                 "10. Critical thinking\n"
+    //                 "EOF";
+                     
+    
+        
+    //	const char *cmd = skills.c_str();
+    //	system(cmd);
+    	
+    //	string instruction = "cat skills.txt";
+    //	const char *command = instruction.c_str();
+    //	system(command);
+    
+    
+    
+    //Task5
+    //string instruction = "head skills.txt -n 7";
+    //const char *command = instruction.c_str();
+    //system(command);
+    
+    
+    //string instruction = "tail skills.txt -n 7";
+    //const char *command = instruction.c_str();
+    //system(command);
+
+    return 0;
+}
---
+++ Lab_07/lab_ass.cpp
@@ -0,0 +1,132 @@
+#include <bits/stdc++.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+
+// calculate the sum of every row
+int get_row_sum(int matrix[3][3], int size, int row) {
+    int sum = 0;
+    for (int j = 0; j < size; ++j) {
+        sum += matrix[row][j];
+    }
+    return sum;
+}
+
+// calculate the sum of every column
+int get_column_sum(int matrix[3][3], int size, int col) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += matrix[i][col];
+    }
+    return sum;
+}
+
+// calculate the sum of main or trailing diagonal
+int get_diagonal_sum(int matrix[3][3], int size, bool main_diagonal) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        if (main_diagonal)
+            sum += matrix[i][i];
+        else
+            sum += matrix[i][size - 1 - i];
+    }
+    return sum;
+}
+
+// check if the square matrix is a magic square or not
+bool is_magic(int matrix[3][3], int size) {
+    int pipe_row[2];
+    int pipe_col[2];
+
+    if (pipe(pipe_row) == -1 || pipe(pipe_col) == -1) {
+        cerr << "pipe can't be created" << endl;
+        return false;
+    }
+
+    pid_t pid_row, pid_col;
+
+    pid_row = fork();
+
+    // fork child to check row sum  
+    if (pid_row == 0) {
+        int row_sum = get_row_sum(matrix, size, 0);
+        for (int i = 1; i < size; ++i) {
+            if (row_sum != get_row_sum(matrix, size, i)) {
+                close(pipe_row[0]);
+                write(pipe_row[1], "-1", sizeof("-1"));
+                close(pipe_row[1]);
+                return false;
+            }
+        }
+        close(pipe_row[0]);
+        write(pipe_row[1], &row_sum, sizeof(row_sum));
+        close(pipe_row[1]);
+        return false;
+    }
+
+    pid_col = fork();
+
+    // fork child to check column sum  
+    if (pid_col == 0) { 
+        int col_sum = get_column_sum(matrix, size, 0);
+        for (int j = 1; j < size; ++j) {
+            if (col_sum != get_column_sum(matrix, size, j)) {
+                close(pipe_col[0]);
+                write(pipe_col[1], "-1", sizeof("-1"));
+                close(pipe_col[1]);
+                return false;
+            }
+        }
+        close(pipe_col[0]);
+        write(pipe_col[1], &col_sum, sizeof(col_sum));
+        close(pipe_col[1]);
+        return false;
+    }
+
+    // parent process
+    close(pipe_row[1]);
+    close(pipe_col[1]);
+
+    int row_sum, col_sum;
+    read(pipe_row[0], &row_sum, sizeof(row_sum));
+    read(pipe_col[0], &col_sum, sizeof(col_sum));
+
+    close(pipe_row[0]);
+    close(pipe_col[0]);
+
+    if (row_sum == -1 || col_sum == -1) {
+        cout << "Input square is not a magic square." << endl;
+        return false;
+    }
+
+    int main_diagonal_sum = get_diagonal_sum(matrix, size, true);
+    int trailing_diagonal_sum = get_diagonal_sum(matrix, size, false);
+
+    if (row_sum == col_sum && row_sum == main_diagonal_sum && row_sum == trailing_diagonal_sum) {
+        cout << "Input square is a magic square." << endl;
+        return true;
+    } else {
+        cout << "Input square is not a magic square." << endl;
+        return false;
+    }
+}
+
+
+int main() {
+    int size = 3;
+    cout << "Let's take a square matrix of size 3x3." << endl;
+
+    int matrix[3][3];
+    cout << "Enter the elements of the matrix:" << endl;
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            cout<< "Enter the element at [" << i+1 << "][" << j+1 << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+
+    is_magic(matrix, size);
+
+    return 0;
+}
