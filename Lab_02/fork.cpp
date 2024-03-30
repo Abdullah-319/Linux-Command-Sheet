9760009fc0dc0ccaa1350d0509340bb334514c88
Abdullah-319 <abdullahahmad319.319@gmail.com>
2024-03-30 16:26:04 +0500

    lab_02 prep done

--- Lab_02/fork.cpp
+++ Lab_02/fork.cpp
@@ -1,29 +1,34 @@
-#include<bits/stdc++.h>
-#include<sys/wait.h>
+#include <bits/stdc++.h>
+#include <sys/wait.h>
 
 using namespace std;
 
-int main(){
-pid_t process = fork();
-
-char choice = 'y';
-
-while (choice == 'y' || choice == 'Y')
+int main()
 {
-if( process == 0){
-cout<<"Child => pid:"<< getpid() << "ppid: " << getppid() << endl;
-exit(EXIT_SUCCESS);
-}
-else if( process > 0){
-cout<<"parent => pid:"<< getpid() << "ppid: " << getppid() << endl;
-sleep(5);
-cout<<"Child run successfully\n";
-}else {
-cout<<"Error creating child";
-}
+    pid_t process = fork();
 
-cout<<"Do you want to run another program (y/n)?? ";
-cin>>choice;
-}
-return 0;
+    char choice = 'y';
+
+    while (choice == 'y' || choice == 'Y')
+    {
+        if (process == 0)
+        {
+            cout << "Child => pid:" << getpid() << "ppid: " << getppid() << endl;
+            exit(EXIT_SUCCESS);
+        }
+        else if (process > 0)
+        {
+            cout << "parent => pid:" << getpid() << "ppid: " << getppid() << endl;
+            sleep(5);
+            cout << "Child run successfully\n";
+        }
+        else
+        {
+            cout << "Error creating child";
+        }
+
+        cout << "Do you want to run another program (y/n)?? ";
+        cin >> choice;
+    }
+    return 0;
 }
